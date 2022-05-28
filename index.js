a3fa3212740eaae101b9721568de54acbd51bc72
Zia Hellman <ziahellman@gmail.com>
2022-05-28 18:16:36 +0300

    commit tree 8d9008532051a712f67c22480c564f4a3b8c23ab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+function appendCat() {
+  const newCats = [...cats, "Broom"];
+  return newCats;
+}
+function prependCat() {
+  const newerCats = ["Arnold", ...cats];
+  return newerCats;
+}
+
+function removeLastCat() {
+  const removedCats = cats.slice(0, cats.length - 1);
+  return removedCats;
+}
+function removeFirstCat() {
+  const firstCat = cats.slice(1);
+  return firstCat;
+}
